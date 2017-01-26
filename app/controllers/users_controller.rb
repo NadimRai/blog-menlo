a1f43368cc95ddfe8fe35fb966cfa378754929fc
NadimRai <rai_nadim@yahoo.com>
2017-01-26 12:40:03 -0800

    Add Admin User functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,25 +11,17 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
-def create
 
-@user = User.new(user_params)
-
-if @user.save
-
-session[:user_id] = @user.id
-
-flash[:success] = "Welcome to the alpha blog #{@user.username}"
-
-redirect_to user_path(@user)
-
-else
-
-render 'new'
-
-end
-
-end
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			session[:user_id] = @user.id
+			flash[:success] = "Welcome to the alpha blog #{@user.username}"
+			redirect_to user_path(@user)
+		else
+			render 'new'
+		end
+	end
 
 	def show
 		
@@ -63,7 +55,7 @@ private
 	end
 
 	def require_same_user
-		if current_user != @user
+		if current_user != @user 
 			flash[:danger] = "You can only edit your own account"
 			redirect_to root_path
 		end
