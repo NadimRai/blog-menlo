c0a02b2040502ff5d75a5fac19dd75f325e1cad3
NadimRai <rai_nadim@yahoo.com>
2017-01-27 16:47:34 -0800

    Ability to create Categories and Display in Index

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -4,9 +4,11 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
 	test "get new category form and create category" do
 		get new_category_path
-		# assert_template 'categories/new'
-		# assert_difference 'Category.count', 1 do
-		# 	post_via_redirect categories_path, category: {name: "sports"}
-		# end
+		assert_template 'categories/new'
+		assert_difference 'Category.count', 1  do
+		follow_redirect! categories_path, params: { category: {name: "sports"}}
+		end
+		assert_template 'categories/index'
+		assert_match "sports", response.body
 	end
 end
